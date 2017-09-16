c19ac8510fdc95c0dc58aa1c1c7afbcd72c283b1
Dominik Bucher <dominik.bucher@gmail.com>
2017-09-16 03:52:35 +0200

    Changed everything so we can deploy to Swisscom.

--- server/server.py
+++ server/server.py
@@ -1,13 +1,16 @@
+import os
 from flask import Flask, send_from_directory
 app = Flask(__name__)
 
+port = int(os.getenv('PORT', '3000'))
+
 @app.route('/')
 def hello1():
-    return send_from_directory('../webapp', 'index.html')
+    return send_from_directory('webapp', 'index.html')
 
 @app.route("/<path:file>")
 def hello(file):
-    return send_from_directory('../webapp', file)
+    return send_from_directory('webapp', file)
 
 if __name__ == '__main__':
-    app.run(port=80)
+    app.run(host='0.0.0.0', port=port)
