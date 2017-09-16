ae35190859e9fe53d68a7907f51ccee33ce424b8
Dominik Bucher <dominik.bucher@gmail.com>
2017-09-16 03:34:43 +0200

    Added bootstrap and jquery.

--- server/server.py
+++ server/server.py
@@ -1,9 +1,13 @@
 from flask import Flask, send_from_directory
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route('/')
+def hello1():
     return send_from_directory('../webapp', 'index.html')
+
+@app.route("/<path:file>")
+def hello(file):
+    return send_from_directory('../webapp', file)
 
 if __name__ == '__main__':
     app.run(port=80)
