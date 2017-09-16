5edb27dbf013192da2621e84430da2daf3363d39
Dominik Bucher <dominik.bucher@gmail.com>
2017-09-16 03:16:50 +0200

    Made flask send stuff from webapp.

--- server/server.py
+++ server/server.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return send_from_directory('../webapp', 'index.html')
+
+if __name__ == '__main__':
+    app.run(port=80)
