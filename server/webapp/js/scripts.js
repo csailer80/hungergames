637d16bc1710cfcae4a45f1eb098cf2174999dda
Dominik Bucher <dominik.bucher@gmail.com>
2017-09-17 01:06:36 +0200

    Added map.

--- server/webapp/js/scripts.js
+++ server/webapp/js/scripts.js
@@ -1,3 +1,4 @@
+var participantColors = [];
 function pollParticipants() {
     var participants;
 
@@ -58,7 +59,7 @@ function pollParticipants() {
         for (i = 0; i < participants.length; i++) {
             var participant = participants[i];
 
-            (function (participant) {
+            (function (participant, i) {
                 $.getJSON('http://hungergames-vested-mayfly.scapp.io/pollParticipantChallenges/' + participant.id, function (challenges) {
                     if (challenges.length > 0) {
                         for (j = 0; j < challenges.length; j++) {
@@ -67,6 +68,11 @@ function pollParticipants() {
                             // color code the participant based on the first challenge
                             if (j == 0) {
                                 $("#" + participant.id).get(0).classList.add("challenge-" + challenge.status);
+                                if (challenge.status === 'INIT') {
+                                  participantColors[i] = '#ffa500';
+                                } else {
+                                  participantColors[i] = '#00ff00';
+                                }
                             }
 
                             $("#" + participant.id + "ChallengesBlock").append(
@@ -78,9 +84,10 @@ function pollParticipants() {
                     } else {
                         // color code the participant to none
                         $("#" + participant.id).get(0).classList.add("challenge-none");
+                        participantColors[i] = '#ff0000';
                     }
                 });
-            })(participant);
+            })(participant, i);
         }
     });
 
@@ -155,3 +162,103 @@ function startChallenge(participantId) {
 
 
 }
+
+var map = L.map("map").setView([46.43266, 9.768719], 15);
+var mapElements = [];
+L.esri.basemapLayer("Imagery").addTo(map);
+
+var trackpoints = [];
+var latlngs = [];
+var pointList = [];
+var offset = 0;
+var seconds = 0;
+
+function incrementSeconds() {
+    for (el of mapElements) {
+        map.removeLayer(el);
+    }
+    mapElements = [];
+
+    function drawOne(time, color) {
+        var diffLat = latlngs[time + 1]["lat"] - latlngs[time]["lat"];
+        var diffLng = latlngs[time + 1]["lon"] - latlngs[time]["lon"];
+
+        var center = [latlngs[time]["lat"] + diffLat / 2, latlngs[time]["lon"] + diffLng / 2];
+        var angle = 360 - (Math.atan2(diffLat, diffLng) * 57.295779513082);
+
+        var arrowM = new L.marker(center, {
+            icon: new L.divIcon({
+                className: "arrowIcon",
+                iconSize: new L.Point(30, 30),
+                iconAnchor: new L.Point(15, 15),
+                html: "<div style = 'font-size: 20px; -webkit-transform: rotate(" + angle + "deg); color:" + color + "'>&#10151;</div>"
+            })
+        }).addTo(map);
+
+        mapElements.push(arrowM);
+    }
+    drawOne(seconds, participantColors[0]);// '#0074D9');
+    if (seconds > 20) {
+        drawOne(seconds - 19, participantColors[1]);//  '#2ECC40');
+    }
+    if (seconds > 30) {
+        drawOne(seconds - 28, participantColors[2]);//  '#FF4136');
+    }
+    if (seconds > 45) {
+        drawOne(seconds - 41, participantColors[3]);//  '#B10DC9');
+    }
+    if (seconds > 75) {
+        drawOne(seconds - 74, participantColors[4]);//  '#FFDC00');
+    }
+    if (seconds > 84) {
+        drawOne(seconds - 83, participantColors[5]);//  '#DCFF00');
+    }
+
+
+    if (seconds % 50 == 0) {
+        var diffLat = latlngs[seconds + 1]["lat"] - latlngs[seconds]["lat"];
+        var diffLng = latlngs[seconds + 1]["lon"] - latlngs[seconds]["lon"];
+
+        var center = [latlngs[seconds]["lat"] + diffLat / 2, latlngs[seconds]["lon"] + diffLng / 2];
+        map.panTo(center);
+    }
+
+    seconds++;
+    if (seconds > latlngs.length) {
+        seconds = 0;
+    }
+    setTimeout(incrementSeconds, 250);
+}
+
+function getResults(offset) {
+    console.log(offset);
+
+    var query = new L.esri.query({
+        url: 'https://services1.arcgis.com/i9MtZ1vtgD3gTnyL/arcgis/rest/services/Refnr144269/FeatureServer/0'
+    });
+    query.where('1=1').orderBy('time').offset(offset).limit(500).run(function (error, results) {
+        if (!error && results.features.length > 0) {
+            for (el of results.features) {
+                var mapEl = L.geoJson(el);
+                trackpoints.push(mapEl);
+                latlngs.push({
+                    'lat': el['properties']['lat'],
+                    'lon': el['properties']['lon']
+                });
+                pointList.push(new L.LatLng(el['properties']['lat'], el['properties']['lon']));
+            }
+            getResults(offset + 500);
+        } else {
+            console.log(error);
+            setTimeout(incrementSeconds, 50);
+            var track = new L.Polyline(pointList, {
+                color: 'red',
+                weight: 3,
+                opacity: 0.5,
+                smoothFactor: 1
+            });
+            track.addTo(map);
+        }
+    });
+}
+getResults(0);
